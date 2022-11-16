74b520ab9aedca9bbc9a3ef413223c23044a43e8
hazki <hazkidev@gmail.com>
2022-11-16 12:11:30 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" });
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+
+    return response.status(201).send();
+});
+
+app.get("/statement/:cpf", (request, response) => {
+    const { cpf } = request.params;
+
+    const customer = customers.find((customer => customer.cpf === cpf));
+
+
+})
+
+app.listen(3333);
+
+//1:34:26
